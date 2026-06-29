e97a047a1a0e1b9753e8a00d6c9e176f2b68f6e0
Aryav <aryav.tyagi@gmail.com>
2026-06-29 16:13:26 -0700

    finished backend

--- backend/src/routes/projects.ts
+++ backend/src/routes/projects.ts
@@ -5,5 +5,14 @@ import * as projectsController from "../controllers/projects";
 const router = Router();
 
 // Check section 1-2 in the README for more details on how to create these routes.
+router.get("/all", projectsController.getAllProjects);
+
+router.get("/:id", projectsController.getProjectByID);
+
+router.post("/", projectsController.createProject);
+
+router.put("/:id", projectsController.updateProjectByID);
+
+router.delete("/:id", projectsController.deleteProject);
 
 export default router;
